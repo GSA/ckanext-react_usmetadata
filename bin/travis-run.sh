6c7bb7090ce17158d438954a2165ed2a0e440ab0
Paul Walker <starsinmypockets@gmail.com>
2020-07-09 10:47:14 -0400

    [ci][l]: Run CI against inventory app

--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -1,16 +1,7 @@
-#!/bin/bash
-set -e
+#!/bin/sh -e
 
-if [ $CKANVERSION == 'master' ]
-then
-   export CKAN_MINOR_VERSION=100
-else
-   export CKAN_MINOR_VERSION=${CKANVERSION##*.}
-fi
-
-if (( $CKAN_MINOR_VERSION >= 9 ))
-then
-   pytest --ckan-ini=subdir/test.ini ckanext/react_usmetadata/tests
-else
-   nosetests --ckan --nologcapture --with-pylons=subdir/test.ini --cover-package=ckanext.react_usmetadata --cover-inclusive --cover-erase --cover-tests
-fi
+echo "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" | sudo tee /etc/default/jetty
+sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
+sudo service jetty restart
+cd /usr/lib/ckan/src/ckanext-react_usmetadata
+nosetests --ckan --nologcapture --with-pylons=test.ini --reset-db --with-coverage --cover-package=ckanext.react_usmetadata --cover-inclusive --cover-erase --cover-tests
