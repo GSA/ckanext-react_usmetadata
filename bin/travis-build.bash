8c694fa0c220c5262ec0508a9a3c50a25561b91c
Paul Walker <starsinmypockets@gmail.com>
2020-07-09 11:20:35 -0400

    [ci][s] ..

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -6,7 +6,7 @@ echo "This is travis-build.bash..."
 
 echo "Installing the packages that CKAN requires..."
 sudo apt-get update -qq
-sudo apt-get install postgresql-$PGVERSION solr-jetty
+sudo apt-get install postgresql-9.6 solr-jetty
 
 echo "Installing CKAN and its Python dependencies..."
 git clone https://github.com/gsa/ckan
